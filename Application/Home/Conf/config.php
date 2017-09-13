12d9b17d504116e48e6a841a3c1ec3695f110baf
wtydreamt <wtydreamt@163.com>
2017-09-13 12:06:34 +0800

    update config

--- Application/Home/Conf/config.php
+++ Application/Home/Conf/config.php
@@ -3,7 +3,7 @@ return array(
 
     'DB_TYPE'   => 'mysql', // 数据库类型
     'DB_HOST'   => '121.42.27.223', // 服务器地址
-    'DB_NAME'   => 'cloud', // 数据库名
+    'DB_NAME'   => 'test', // 数据库名
     'DB_USER'   => 'root', // 用户名
     'DB_PWD'    => 'root', // 密码
     'DB_PORT'   => 3306, // 端口
