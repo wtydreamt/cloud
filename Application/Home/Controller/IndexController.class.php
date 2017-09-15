05f8afcfebb5cf5e6b1171debc9fc4016af372bb
wtydreamt <wtydreamt@163.com>
2017-09-15 15:06:29 +0800

    v

--- Application/Home/Controller/IndexController.class.php
+++ Application/Home/Controller/IndexController.class.php
@@ -12,6 +12,8 @@ class IndexController extends Controller {
     }
     public function index(){
 		   if($this->admin){
+              $res=M("product_name")->select();
+              $this->assign("product",$res);
 		   	  $this->display("release");
 		   }else{
 		   	  $this->display("login");
@@ -19,24 +21,41 @@ class IndexController extends Controller {
     }
 
     public function login(){
+            $host=$_SERVER['SERVER_NAME'];
     		$data=I();
     		$user=trim($data['user']);
     		$pwd =trim($data['pwd']);
     		if($user!=$this->logininfo['name']){
     			echo "<script>
     			alert('登录名不正确');
-    			window.location.href='http://lei.x-cloud.cc'
+    			window.location.href=".$host."
     			</script>";die;
     		}    		
     		if(md5($pwd)!=$this->logininfo['pwd']){
     			echo "<script>
     			alert('密码不正确');
-    			window.location.href='http://lei.x-cloud.cc'
+    			window.location.href=".$host."
     			</script>";die;
     		}
     		cookie("admin",$user);
     		echo "<script>
-    			window.location.href='http://lei.x-cloud.cc'
+    			window.location.href=".$host."
     			</script>";
+    }
+
+    public function addproduct(){
+        $name=I("name");
+        $res=M("product_name")->where(array("name"=>$name))->find();
+        if($res){
+            echo "此产品名已存在";die;
+        }else{
+            $res_add=M("product_name")->add(array("name"=>$name));
+            if($res_add){
+                echo "ok";
+            }else{
+                echo "添加失败";
+            }
+        }
+
     }
 }
